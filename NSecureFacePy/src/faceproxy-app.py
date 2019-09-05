3e758c63469e2f0663673b020372c28ee8b535e2
Jun Liu <liu.jun@outlook.com>
2019-09-05 10:26:55 +0800

    update proxy server

--- NSecureFacePy/src/faceproxy-app.py
+++ NSecureFacePy/src/faceproxy-app.py
@@ -7,20 +7,7 @@ async def handler(request):
     return web.Response(text="RECEIVED")
 
 
-async def main():
-    server = web.Server(handler=handler)
-    runner = web.ServerRunner(server)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 5001)
-    await site.start()
+app = web.Application()
+app.add_routes([web.get('/', handler)])
 
-    await asyncio.sleep(100*3600)
-
-loop = asyncio.get_event_loop()
-
-try:
-    loop.run_until_complete(main())
-except KeyboardInterrupt:
-    pass
-
-loop.close()
+web.run_app(app, host='0.0.0.0', port=5001)
